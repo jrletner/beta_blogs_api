c2364993088e4c53b7584ac55a33e5cc05be51d8
Joe Letner <jrletner@gmail.com>
2025-07-08 18:04:55 -0500

    Implement UsersController with CRUD actions and strong parameters

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
+  end
+
+private
+
+def user_params
+  params.permit(:first_name, :last_name, :username, :email)
+end
+end
