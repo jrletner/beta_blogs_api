ddf777c0d1af39a0d7c8db716d139b180cdc3232
Joe Letner <jrletner@gmail.com>
2025-07-10 17:57:18 -0500

    Refactor seed data generation and remove Profiles from Postman collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 require 'faker'
 
-# Clear existing data (optional, for dev/test only)
-Profile.destroy_all
 Blog.destroy_all
 Category.destroy_all
 User.destroy_all
+Profile.destroy_all
 
-# Seed Categories
+
+# seed categories
 categories = []
 5.times do
   categories << Category.create!(name: Faker::Book.unique.genre)
 end
 
-# Seed Users & Profiles
+# seed users
 users = []
 10.times do
-  user = User.create!(
+  users << User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     username: Faker::Internet.unique.username,
     email: Faker::Internet.unique.email
   )
-  # Profile is created automatically via after_create
-  user.profile.update!(
-    bio: Faker::Lorem.sentence(word_count: 10),
-    avatar_url: Faker::Avatar.image
-  )
-  users << user
 end
 
-# Seed Blogs (with categories)
+# seed blogs (with category)
 20.times do
   blog = Blog.create!(
     title: Faker::Book.title,
