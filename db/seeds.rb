111ba9bda4b56c17ec83b755a9c923b709bc2dd0
Joe Letner <jrletner@gmail.com>
2025-07-10 17:16:01 -0500

    Add seed data generation for profiles, users, categories, and blogs using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# Clear existing data (optional, for dev/test only)
+Profile.destroy_all
+Blog.destroy_all
+Category.destroy_all
+User.destroy_all
+
+# Seed Categories
+categories = []
+5.times do
+  categories << Category.create!(name: Faker::Book.unique.genre)
+end
+
+# Seed Users & Profiles
+users = []
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.unique.username,
+    email: Faker::Internet.unique.email
+  )
+  # Profile is created automatically via after_create
+  user.profile.update!(
+    bio: Faker::Lorem.sentence(word_count: 10),
+    avatar_url: Faker::Avatar.image
+  )
+  users << user
+end
+
+# Seed Blogs (with categories)
+20.times do
+  blog = Blog.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 5),
+    user: users.sample
+  )
+  blog.categories << categories.sample(rand(1..3))
+end
