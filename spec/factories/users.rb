3cd55f46efbbeaf78dd1c25d31dc385c22a87bc3
Joe Letner <jrletner@gmail.com>
2025-07-12 16:12:18 -0500

    Remove password and password_confirmation fields from user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,5 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     username { Faker::Internet.username }
-    password { 'password' }
-    password_confirmation { 'password' }
   end
 end
