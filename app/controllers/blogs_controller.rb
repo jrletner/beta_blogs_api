a92e9caf171c2a016187d76457bc8560f1f3d8f2
Joe Letner <jrletner@gmail.com>
2025-07-14 18:55:07 -0500

    Implement user authentication with JWT and add user validations

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :authenticate_request, except: [ :show ]
   def index
     blogs = Blog.all
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "sessions/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,4 +11,6 @@ Rails.application.routes.draw do
 
 # Beta Blog API Routes
 resources :blogs
+resources :users
+post "/login", to: "sessions#create"
 end
