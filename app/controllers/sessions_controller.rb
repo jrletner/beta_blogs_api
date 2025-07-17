31022e4ab847c6bc7c2f1d352b68d3829aa8f05c
Joe Letner <jrletner@gmail.com>
2025-07-17 15:26:17 -0500

    Add debug output for JWT expiration time in jwt_encode method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
 
   def jwt_encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
+    puts exp.to_i
     JWT.encode(payload, Rails.application.credentials.secret_key_base)
   end
 end
